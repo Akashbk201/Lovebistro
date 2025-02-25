080ca03d3d16a0ccd3c4150dac3e292acf2500d5
Md.Naimur Rahman <74141098+Akashbk201@users.noreply.github.com>
2025-02-25 07:25:10 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+// const imgBox = document.querySelector(".hero-im-box");
+// const vecReview = document.querySelector(".vec-box");
+
+// imgBox.addEventListener("mouseover", () => {
+//   vecReview.classList.remove("hidden");
+// });
+
+// imgBox.addEventListener("mouseout", () => {
+//   vecReview.classList.add("hidden");
+// });
